d0da95dd3c01e31b06947848176ba32e2d5d4c27
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-12-07 22:52:05 +0300

    lock boxes interview question

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,11 +15,3 @@ def canUnlockAll(boxes):
                 opened[key] = True
                 boxList.append(key)
     return all(opened)
-
-
-# boxes1 = [[1], [2], [3], [4], []]
-# print(canUnlockAll(boxes1))
-# boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-# print(canUnlockAll(boxes2))
-# boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-# print(canUnlockAll(boxes3))
