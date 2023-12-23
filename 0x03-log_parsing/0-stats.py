6fccf88c875a88fd766ab0507685cdde0f75861f
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-12-23 12:15:56 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -17,29 +17,29 @@ def main():
 
     try:
         for line in sys.stdin:
-            # Split the line into components
+            """Split the line into components"""
             parts = line.split()
             if len(parts) >= 8:
                 ip, _, _, _, _, status_code, file_size = parts[:7]
 
-                # Check if status_code is a valid integer
+                """Check if status_code is a valid integer"""
                 try:
                     status_code = int(status_code)
                 except ValueError:
                     continue
 
-                # Update metrics
+                """Update metrics"""
                 total_size += int(file_size)
                 status_counts[status_code] += 1
 
                 lines_processed += 1
 
-                # Print statistics every 10 lines
+                """Print statistics every 10 lines"""
                 if lines_processed % 10 == 0:
                     print_statistics(total_size, status_counts)
 
     except KeyboardInterrupt:
-        # Handle Ctrl+C interruption
+        """Handle Ctrl+C interruption"""
         print_statistics(total_size, status_counts)
 
 
